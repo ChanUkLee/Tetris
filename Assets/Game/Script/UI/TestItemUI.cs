94a4a2de7a821989524e710f1a5871d34e89d0e8
ChanUkLee <chanuklee0227@gmail.com>
2016-07-27 17:56:07 +0900

    Test project
    Add test list ui

---
+++ Assets/Game/Script/UI/TestItemUI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Text;
+using System.Collections;
+
+public class TestItemUI : BaseUI {
+
+	public override void Initialize () {
+		base.Initialize ();
+
+		SetText ("TestText", GameStringManager.Instance.GetString("test_item"));
+	}
+}
--- Assets/Game/Script/UI/TestUI.cs
+++ Assets/Game/Script/UI/TestUI.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class TestUI : BaseUI {
+
+	private int _count = 10;
+	private List<TestItemUI> _itemList = new List<TestItemUI> ();
 
 	public override void Initialize () {
 		base.Initialize ();
@@ -9,6 +13,20 @@ public class TestUI : BaseUI {
 		SetText ("TitleText", GameStringManager.Instance.GetString ("test_title"));
 		SetText ("TestText", GameStringManager.Instance.GetString ("test_button"));
 		SetButton ("TestButton", Test);
+
+		GameObject instant = null;
+		TestItemUI component = null;
+		RectTransform content = GetRectTransform ("Content");
+		for (int i = 0; i < this._count; i++) {
+			instant = GameResourceManager.Instance.CreateUI ("test_item_ui");
+			if (instant != null) {
+				instant.transform.SetParent (content);
+				component = instant.GetComponent<TestItemUI> ();
+				if (component != null) {
+					component.Initialize ();
+				}
+			}
+		}
 	}
 
 	private void Test() {
